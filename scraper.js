9d0cdec4f13e1f3fbc8a7c8a2ac450221e0164ef
Mumin Mian <m.mian0122@gmail.com>
2018-08-17 00:35:18 -0400

    Test some solutions

--- scraper.js
+++ scraper.js
@@ -2,9 +2,11 @@
 const fs = require('fs');
 // Package Requirements
 const request = require('request');
+const rp = require('request-promise');
 const cheerio = require('cheerio');
-// const csv = require('json2csv');
-const csvParse = require('json2csv').Parser;
+const json2csvParser = require('json2csv').Parser;
+  // Fields for JSON2CSV
+
 
 // Check for data folder
 function checkDir(path) {
@@ -16,58 +18,104 @@ function checkDir(path) {
 };
 checkDir('./data');
 
-// Generate current date (for filename)
-function generateDate() {
-  let today = new Date();
-  return `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`;
-};
+let productArray= [];
 
-// Create CSV
-function createCSV(path) {
-  fs.writeFileSync(path)
-};
-createCSV(`./data/${generateDate()}.csv`)
-// Fields for JSON2CSV
-const fields = ['Title', 'Price', 'ImageURL', 'URL', 'Time'];
-
-// Request function for each product page
 function productRequest(uri) {
-  request(`http://shirts4mike.com/${uri}`, function(error, response, html) {
-    if (!error && response.statusCode === 200) {
-      let $ = cheerio.load(html);
+  // declare options object for request-promise
+  let options = {
+    uri: `http://shirts4mike.com/${uri}`,
+    transform: function(body) {
+      return cheerio.load(body)
+    }
+  };
+  // call request promise
+  rp(options)
+    .then(function(res) {
+      let $ = cheerio.load(`http://shirts4mike.com/${uri}`);
       let productInfo = {
         title : $('div.shirt-details > h1')
           .contents()
           .filter(function() {
-              return this.nodeType == 3;
+            return this.nodeType == 3;
           })
           .text()
           .slice(1),
         price : $('div.shirt-details > h1 > span.price').text(),
-        imgUrl : $('div.shirt-picture > span > img').attr('src'),
+        imageUrl : $('div.shirt-picture > span > img').attr('src'),
         url : `http://shirts4mike.com/${uri}`
-      };
-      let productJSON = JSON.stringify(productInfo);
-      console.log(productJSON);
-      return productJSON;
-    }
-  });
+      }
+      productArray.push(productInfo)
+    })
+    .then($ => {
+      console.log(productArray);
+    })
 };
+productRequest('shirt.php?id=101');
 
-
-//  Make request
-request('http://shirts4mike.com/shirts.php', function(error, response, html) {
-  if (!error && response.statusCode === 200) {
-    let $ = cheerio.load(html);
-    // Create array of all product page URIs
-    let linksArr = [];
-    const $productLinks = $('div.shirts').find('a');
-    $productLinks.each(i => linksArr[i] = $productLinks[`${i}`].attribs.href);
-    const Json2CsvParse = new csvParse({ fields });
-    // Call requests on each product pages
-    linksArr.forEach(link => {
-      let productData = productRequest(link);
-      console.log(productData);
-    });
-  }
-});
+// // Generate current date (for filename)
+// function generateDate() {
+//   let today = new Date();
+//   return `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`;
+// };
+//
+// // Create CSV
+// function createCSV(path) {
+//   fs.writeFileSync(path)
+// };
+// createCSV(`./data/${generateDate()}.csv`)
+//
+// // Create CSV Parse
+// var JSONArray = [];
+// // Request function for each product page
+// function productRequest(uri) {
+//   request(`http://shirts4mike.com/${uri}`, function(error, response, html) {
+//     if (!error && response.statusCode === 200) {
+//       let $ = cheerio.load(html);
+//       let productInfo = {
+//         title : $('div.shirt-details > h1')
+//           .contents()
+//           .filter(function() {
+//               return this.nodeType == 3;
+//           })
+//           .text()
+//           .slice(1),
+//         price : $('div.shirt-details > h1 > span.price').text(),
+//         imageUrl : $('div.shirt-picture > span > img').attr('src'),
+//         url : `http://shirts4mike.com/${uri}`
+//       };
+//
+//       JSONArray.push(productInfo);
+//       // console.log(JSONArray);
+//     };
+//   });
+// };
+// try {
+//   productRequest(`shirt.php?id=101`)}
+//
+// console.log(JSONArray);
+// // let data = [
+// //   { yo: 'hey',
+// //     no: 'way',
+// //     ha: 'tay'
+// //   }
+// // ]
+// const fields = ['title', 'price', 'imageUrl', 'url'];
+// const opts = { fields };
+// //
+// const json2csv = new json2csvParser({ fields });
+// const csv = json2csv.parse(JSONArray);
+// console.log(csv);
+//
+// // //  Make request
+// // request('http://shirts4mike.com/shirts.php', function(error, response, html) {
+// //   if (!error && response.statusCode === 200) {
+// //     let $ = cheerio.load(html);
+// //     // Create array of all product page URIs
+// //     let linksArr = [];
+// //     const $productLinks = $('div.shirts').find('a');
+// //     $productLinks.each(i => linksArr[i] = $productLinks[`${i}`].attribs.href);
+// //     // Call requests on each product pages
+// //     console.log(linksArr);
+// //     $(linksArr).each(i => console.log(productRequest(linksArr[i])));
+// //   }
+// // });
